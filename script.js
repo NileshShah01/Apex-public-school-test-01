5ce7e823f170f995e6a335c095c0accbf918b713
NileshShah01 <Nileshshah84870@gmail.com>
2026-03-11 04:06:18 +0530

    Update script.js

--- script.js
+++ script.js
@@ -22,37 +22,20 @@ function initMenu()
 const toggle = document.getElementById("menuToggle");
 const menu = document.getElementById("menu");
 const overlay = document.getElementById("menuOverlay");
-touchStartX = 0
-touchEndX = 0
 
-menu.addEventListener("touchstart", function(e)
-{
-touchStartX = e.changedTouches[0].screenX
-})
+if(!toggle || !menu || !overlay) return;
 
-menu.addEventListener("touchend", function(e)
-{
-touchEndX = e.changedTouches[0].screenX
-
-if(touchStartX - touchEndX > 70)
-{
-menu.classList.remove("active")
-overlay.classList.remove("active")
-}
-})
-if(!toggle || !menu) return;
-
-toggle.onclick = function()
+toggle.addEventListener("click", function()
 {
 menu.classList.toggle("active");
 overlay.classList.toggle("active");
-};
+});
 
-overlay.onclick = function()
+overlay.addEventListener("click", function()
 {
 menu.classList.remove("active");
 overlay.classList.remove("active");
-};
+});
 }
 /* HERO SLIDER */
 
@@ -92,15 +75,7 @@ showSlide(currentSlide);
 /* AUTO SLIDER */
 
 setInterval(nextSlide,3000);
-.menu
-{
-transition:left 0.35s ease;
-}
-.menu a.active
-{
-color:#2563EB;
-font-weight:700;
-}
+
 
 /* COUNTER ANIMATION – FIXED VERSION */
 const counters = document.querySelectorAll(".counter");
@@ -213,6 +188,7 @@ header.style.boxShadow = "0 2px 8px rgba(0,0,0,0.08)";
 }
 
 });
+
 
 
 
